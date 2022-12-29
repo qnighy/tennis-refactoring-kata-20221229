608f4e9a95ebdb23a21cc35fd7046addd5d31a8e
Masaki Hara <ackie.h.gmai@gmail.com>
2022-12-29 15:21:54 +0900

    Add more tests

--- tests/unit.rs
+++ tests/unit.rs
@@ -32,6 +32,31 @@ fn test_game3() {
 }
 
 #[test]
+fn test_game14() {
+    test_game(0, 3, "Love-Forty");
+}
+
+#[test]
+fn test_game15() {
+    test_game(1, 0, "Fifteen-Love");
+}
+
+#[test]
+fn test_game6() {
+    test_game(1, 1, "Fifteen-All");
+}
+
+#[test]
+fn test_game16() {
+    test_game(1, 2, "Fifteen-Thirty");
+}
+
+#[test]
+fn test_game17() {
+    test_game(1, 3, "Fifteen-Forty");
+}
+
+#[test]
 fn test_game4() {
     test_game(2, 0, "Thirty-Love");
 }
@@ -42,10 +67,56 @@ fn test_game5() {
 }
 
 #[test]
-fn test_game6() {
-    test_game(1, 1, "Fifteen-All");
-}
-#[test]
 fn test_game7() {
     test_game(2, 2, "Thirty-All");
+}
+
+#[test]
+fn test_game18() {
+    test_game(2, 3, "Thirty-Forty");
+}
+
+#[test]
+fn test_game19() {
+    test_game(3, 0, "Forty-Love");
+}
+
+#[test]
+fn test_game20() {
+    test_game(3, 1, "Forty-Fifteen");
+}
+
+#[test]
+fn test_game21() {
+    test_game(3, 2, "Forty-Thirty");
+}
+
+#[test]
+fn test_game8() {
+    test_game(3, 3, "Deuce");
+}
+
+#[test]
+fn test_game9() {
+    test_game(4, 4, "Deuce");
+}
+
+#[test]
+fn test_game10() {
+    test_game(4, 5, "Advantage player2");
+}
+
+#[test]
+fn test_game11() {
+    test_game(5, 4, "Advantage player1");
+}
+
+#[test]
+fn test_game12() {
+    test_game(4, 6, "Win for player2");
+}
+
+#[test]
+fn test_game13() {
+    test_game(6, 4, "Win for player1");
 }
